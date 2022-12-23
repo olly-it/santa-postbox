835b17dc252e2bbf5e8ad445bd25bd88bdd994fa
Alessio Olivieri <alessio.olivieri@zodiakactive.com>
2022-12-23 18:12:40 +0100

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// material stuff
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FileUploadComponent,
-    SearchFormComponent
-  ],
+  declarations: [AppComponent, FileUploadComponent, SearchFormComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    //material modules
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,30 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
-
+import { DateAdapter } from '@angular/material/core';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+  styleUrls: ['./search-form.component.css'],
 })
 export class SearchFormComponent implements OnInit {
-
-  me:any;
-  loaded:boolean = false;
-  constructor(private firebaseService: FirebaseService) {}
-
-  ngOnInit(): void {
+  me: any;
+  loaded: boolean = false;
+  constructor(
+    private firebaseService: FirebaseService,
+    private dateAdapter: DateAdapter<any>
+  ) {
+    this.dateAdapter.setLocale('en-GB'); // dd/mm/yyyy
   }
-  search(name:string, birthDate:string): void {
-    console.log("looking for: ",name,birthDate);
-    this.firebaseService.getMe(name, birthDate).then(x=> {
-      this.me=x;
-      console.log("me: ",this.me);
-      this.loaded=true;
+
+  ngOnInit(): void {}
+  search(name: string, birthDate: string): void {
+    console.log('looking for: ', name, birthDate);
+    if (!name || !birthDate) return;
+
+    this.firebaseService.getMe(name, birthDate).then((x) => {
+      this.me = x;
+      console.log('me: ', this.me);
+      this.loaded = true;
     });
   }
 
-  add(name:string, birthDate:string, address:string): void {
-    console.log("looking for: ",name,birthDate);
+  add(name: string, birthDate: string, address: string): void {
+    console.log('looking for: ', name, birthDate);
     this.firebaseService.add(name, birthDate, address);
     this.search(name, birthDate);
   }
